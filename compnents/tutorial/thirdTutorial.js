51f9292fe428673629606a77cd30065690623484
Freem11 <freem1985@gmail.com>
2023-08-26 14:19:35 -0600

    tutorial text print working

--- compnents/tutorial/thirdTutorial.js
+++ compnents/tutorial/thirdTutorial.js
@@ -117,6 +117,7 @@ export default function ThirdTutorial() {
   const text26 = "";
 
   const [textRead, setTextRead] = useState("");
+  const [textPrinting, setTextPrinting] = useState(true);
 
   const feederArray = [
     text0,
@@ -155,38 +156,59 @@ export default function ThirdTutorial() {
       return;
     } else {
     if (pushVal === 1 && itterator3 < feederArray.length - 1) {
-      setItterator3((prev) => prev + pushVal);
+      if (textPrinting) {
+        setTextPrinting(false);
+        textArray = "";
+        setTextRead("");
+        setTextRead(feederArray[itterator3]);
+      } else {
+        setItterator3((prev) => prev + pushVal);
+        setTextPrinting(true);
+      }
       }
 
       if (pushVal === 1 && itterator3 === feederArray.length - 1) {
         setThirdGuideModal(false);
       }
     }
-
-    // setTextRead("");
-    // clearInterval(interval);
-    // let textArray = textVal.split("");
-
-    //   interval = setInterval(() => {
-
-    //     setTextRead((prev) => prev + textArray[0]);
-    //     textArray = textArray.slice(1);
-
-    //     if(pushVal === 1){
-    //       clearInterval(interval);
-    //       setTextRead(textVal);
-    //     }
-
-    //     if (!textArray.length) {
-    //       clearInterval(interval);
-    //     }
-
-    //   }, 50);
   };
 
+  let textArray;
+
+  function printOutText() {
+    if (textArray.length > 0) {
+      setTextRead((prev) => prev + textArray[0]);
+      textArray = textArray.slice(1);
+    } else {
+      setTextPrinting(false);
+    }
+  }
+
+  function cleanUp() {
+    clearInterval(textPrinter);
+  }
+
+  let textPrinter;
   useEffect(() => {
+    setTextRead("");
+
     let textVal = feederArray[itterator3];
-    setTextRead(textVal);
+    if (textVal) {
+      textArray = textVal.split("");
+      if (textPrinting) {
+        textPrinter = setInterval(printOutText, 40);
+      } else {
+        setTextRead(textVal);
+      }
+    }
+
+    return () => cleanUp();
+  }, [itterator3, textPrinting]);
+
+
+  useEffect(() => {
+    // let textVal = feederArray[itterator3];
+    // setTextRead(textVal);
 
     if (itterator3 === 0) {
       setTimeout(() => {
@@ -464,7 +486,7 @@ const styles = StyleSheet.create({
   textBox: {
     position: "absolute",
     width: "90%",
-    height: "14%",
+    height: "15%",
     backgroundColor: "white",
     borderRadius: 15,
     alignSelf: "center",
@@ -473,7 +495,7 @@ const styles = StyleSheet.create({
   textContain: {
     padding: 10,
     fontFamily: "SanFran",
-    fontSize: scale(12),
+    fontSize: 13,
   },
   buttonwrapper: {
     flex: 1,
