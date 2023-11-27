b010e6cb18ae40d6d484943431d1169aa023f8c7
Freem11 <freem1985@gmail.com>
2023-11-27 16:39:00 -0700

    pre hackathon reversion

--- compnents/modals/photoBoxModal.js
+++ compnents/modals/photoBoxModal.js
@@ -12,7 +12,6 @@ import Animated, {
   useAnimatedStyle,
   withTiming,
   withDelay,
-  Easing,
 } from "react-native-reanimated";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import React, { useState, useEffect } from "react";
@@ -23,13 +22,6 @@ import * as FileSystem from "expo-file-system";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
-
-const photoWidth = windowWidth - windowWidth * 0.15;
-const photoHeight = windowHeight - windowHeight * 0.15;
-export const clamp = (value, lowerBound, upperBound) => {
-  "worklet";
-  return Math.min(Math.max(lowerBound, value), upperBound);
-};
 
 export default function PhotoBoxModal(props) {
   const { picData, togglePhotoBoxModal } = props;
@@ -67,7 +59,7 @@ export default function PhotoBoxModal(props) {
   const xOffset = useSharedValue(0);
   const yOffset = useSharedValue(0);
 
-  const context = useSharedValue({ x: 0, y: 0, fx: 0, fy: 0 });
+  const context = useSharedValue({ x: 0, y: 0, fx: 0, fy: 0})
 
   const animateTaps = Gesture.Tap()
     .maxDistance(10)
@@ -89,74 +81,14 @@ export default function PhotoBoxModal(props) {
 
   const animatePan = Gesture.Pan()
     .onStart(() => {
-      context.value = {
-        x: xCurrent.value,
-        y: yCurrent.value,
-        fx: focalX.value,
-        fy: focalY.value,
-      };
+      context.value = { x: xCurrent.value, y: yCurrent.value, fx: focalX.value, fy: focalY.value };
     })
     .onUpdate((event) => {
- 
       xCurrent.value = event.translationY + context.value.x;
       yCurrent.value = -event.translationX + context.value.y;
 
       xOffset.value = event.translationY + context.value.x;
       yOffset.value = -event.translationX + context.value.y;
-    })
-    .onEnd(() => {
-      if (
-        -xCurrent.value >
-          windowHeight * 0.75 + (scalePrevious.value - 1) * 100 ||
-        xCurrent.value > windowHeight * 0.75 + (scalePrevious.value - 1) * 100
-      ) {
-        if (xCurrent.value < 0) {
-          xCurrent.value = withTiming(
-            -(windowHeight * 0.7) - (scalePrevious.value - 1) * 100,
-            {
-              duration: 400,
-              easing: Easing.inOut(Easing.ease),
-            }
-          );
-        } else {
-          xCurrent.value = withTiming(
-            windowHeight * 0.7 + (scalePrevious.value - 1) * 100,
-            {
-              duration: 400,
-              easing: Easing.inOut(Easing.ease),
-            }
-          );
-        }
-      } else {
-        xCurrent.value = xCurrent.value;
-      }
-
-      if (
-        yCurrent.value > (photoWidth / 2) * scalePrevious.value ||
-        yCurrent.value < -(photoWidth / 2) * scalePrevious.value
-      ) {
-        if (yCurrent.value < 0) {
-          yCurrent.value = withTiming(
-            -windowWidth * scalePrevious.value * 0.55 +
-              scalePrevious.value * 100,
-            {
-              duration: 400,
-              easing: Easing.inOut(Easing.ease),
-            }
-          );
-        } else {
-          yCurrent.value = withTiming(
-            windowWidth * scalePrevious.value * 0.55 -
-              scalePrevious.value * 100,
-            {
-              duration: 400,
-              easing: Easing.inOut(Easing.ease),
-            }
-          );
-        }
-      } else {
-        yCurrent.value = yCurrent.value;
-      }
     });
 
   const animatePicPinch = Gesture.Pinch()
@@ -167,7 +99,6 @@ export default function PhotoBoxModal(props) {
       }
     })
     .onUpdate((event) => {
-
       if (event.numberOfPointers == 2) {
         // On Android, the onStart event gives 0,0 for the focal
         // values, so we set them here instead too.
@@ -175,194 +106,20 @@ export default function PhotoBoxModal(props) {
           focalX.value = event.focalX;
           focalY.value = event.focalY;
         }
-
         scaleCurrent.value = event.scale;
 
-        xCurrent.value =
-          (1 - scaleCurrent.value) * (focalX.value - windowWidth / 2) +
-          xOffset.value;
-        yCurrent.value =
-          (1 - scaleCurrent.value) * (focalY.value - windowHeight / 2) +
-          yOffset.value;
+        xCurrent.value = (1 - scaleCurrent.value) * (focalX.value - windowWidth / 2) + xOffset.value;
+        yCurrent.value = (1 - scaleCurrent.value) * (focalY.value - windowHeight / 2) + yOffset.value;
       }
     })
     .onEnd(() => {
+      scalePrevious.value = scalePrevious.value * scaleCurrent.value;
 
-      if (scalePrevious.value * scaleCurrent.value < 1) {
-        scalePrevious.value = withTiming(1, {
-          duration: 500,
-          easing: Easing.inOut(Easing.ease),
-        });
-        console.log("A", yCurrent.value)
-        if (
-          yCurrent.value > (photoWidth / 2) * scaleCurrent.value ||
-          yCurrent.value < -(photoWidth / 2) * scaleCurrent.value
-        ) {
-          if (yCurrent.value < 0) {
-            yCurrent.value = withTiming(
-              -windowWidth * scaleCurrent.value * 0.55 +
-                scaleCurrent.value * 100,
-              {
-                duration: 400,
-                easing: Easing.inOut(Easing.ease),
-              }
-            );
-            yPrevious.value = 0;
-          } else {
-            yCurrent.value = withTiming(
-              windowWidth * scaleCurrent.value * 0.55 -
-                scaleCurrent.value * 100,
-              {
-                duration: 400,
-                easing: Easing.inOut(Easing.ease),
-              }
-            );
-            yPrevious.value = 0;
-          }
-        } else {
-          yPrevious.value =
-            scaleCurrent.value * yPrevious.value + yCurrent.value;
-          yCurrent.value = 0;
-        }
+      xPrevious.value = scaleCurrent.value * xPrevious.value + xCurrent.value;
+      yPrevious.value = scaleCurrent.value * yPrevious.value + yCurrent.value;
 
-        console.log("B", xCurrent.value)
-        if (
-          -xCurrent.value >
-            windowHeight * 0.5 + (scaleCurrent.value - 1) * 100 ||
-          xCurrent.value > windowHeight * 0.5 + (scaleCurrent.value - 1) * 100
-        ) {
-          if (xCurrent.value < 0) {
-            xCurrent.value =
-              scaleCurrent.value * xCurrent.value +
-              (-(windowHeight * 0.7) + (scaleCurrent.value - 1) * 100);
-          } else {
-            xCurrent.value =
-              scaleCurrent.value * xCurrent.value +
-              windowHeight * 0.7 +
-              (scaleCurrent.value - 1) * 100;
-          }
-          xPrevious.value = 0;
-        } else {
-          xPrevious.value =
-            scaleCurrent.value * xPrevious.value + xCurrent.value;
-          xCurrent.value = 0;
-        }
-      } else if (scalePrevious.value * scaleCurrent.value > 4) {
-        scalePrevious.value = withTiming(4, {
-          duration: 500,
-          easing: Easing.inOut(Easing.ease),
-        });
-        console.log("C", yCurrent.value)
-        if (
-          yCurrent.value > (photoWidth / 2) * scaleCurrent.value ||
-          yCurrent.value < -(photoWidth / 2) * scaleCurrent.value
-        ) {
-          if (yCurrent.value < 0) {
-            yCurrent.value = withTiming(
-              -windowWidth * scaleCurrent.value * 0.55 +
-                scaleCurrent.value * 100,
-              {
-                duration: 400,
-                easing: Easing.inOut(Easing.ease),
-              }
-            );
-            yPrevious.value = 0;
-          } else {
-            yCurrent.value = withTiming(
-              windowWidth * scaleCurrent.value * 0.55 -
-                scaleCurrent.value * 100,
-              {
-                duration: 400,
-                easing: Easing.inOut(Easing.ease),
-              }
-            );
-            // yPrevious.value = 0;
-          }
-        } else {
-          yPrevious.value =
-            scaleCurrent.value * yPrevious.value + yCurrent.value;
-          yCurrent.value = 0;
-        }
-        console.log("D", xCurrent.value)
-        if (
-          -xCurrent.value >
-            windowHeight * 0.75 + (scaleCurrent.value - 1) * 100 ||
-          xCurrent.value > windowHeight * 0.75 + (scaleCurrent.value - 1) * 100
-        ) {
-          if (xCurrent.value < 0) {
-            xCurrent.value =
-              scaleCurrent.value * xCurrent.value +
-              (-(windowHeight * 0.7) + (scaleCurrent.value - 1) * 100);
-          } else {
-            xCurrent.value =
-              scaleCurrent.value * xCurrent.value +
-              windowHeight * 0.7 +
-              (scaleCurrent.value - 1) * 100;
-          }
-          // xPrevious.value = 0;
-        } else {
-          xPrevious.value =
-            scaleCurrent.value * xPrevious.value + xCurrent.value;
-          xCurrent.value = 0;
-        }
-      } else {
-        scalePrevious.value = scalePrevious.value * scaleCurrent.value;
-        console.log("E", yCurrent.value)
-        if (
-          yCurrent.value > (photoWidth / 2) * scaleCurrent.value ||
-          yCurrent.value < -(photoWidth / 2) * scaleCurrent.value
-        ) {
-          if (yCurrent.value < 0) {
-            yCurrent.value = withTiming(
-              -windowWidth * scaleCurrent.value * 0.55 +
-                scaleCurrent.value * 100,
-              {
-                duration: 400,
-                easing: Easing.inOut(Easing.ease),
-              }
-            );
-            yPrevious.value = 0;
-          } else {
-            yCurrent.value = withTiming(
-              windowWidth * scaleCurrent.value * 0.55 -
-                scaleCurrent.value * 100,
-              {
-                duration: 400,
-                easing: Easing.inOut(Easing.ease),
-              }
-            );
-            // yPrevious.value = 0;
-          }
-        } else {
-          yPrevious.value =
-            scaleCurrent.value * yPrevious.value + yCurrent.value;
-          yCurrent.value = 0;
-        }
-        console.log("F", xCurrent.value, windowHeight * 0.5 + (scaleCurrent.value) * 100)
-        if (
-          -xCurrent.value >
-            windowHeight * 0.5 - (scaleCurrent.value) * 100 ||
-          xCurrent.value > windowHeight * 0.5 - (scaleCurrent.value) * 100
-        ) {
-          if (xCurrent.value < 0) {
-            console.log("extra", scaleCurrent.value * xCurrent.value +
-            (-(windowHeight * 0.7) + (scaleCurrent.value) * 200))
-            xCurrent.value =
-              scaleCurrent.value * xCurrent.value +
-              (-(windowHeight * 0.7) + (scaleCurrent.value) * 200);
-          } else {
-            xCurrent.value =
-              scaleCurrent.value * xCurrent.value +
-              windowHeight * 0.7 +
-              (scaleCurrent.value) * 100;
-          }
-          // xPrevious.value = 0;
-        } else {
-          xPrevious.value =
-            scaleCurrent.value * xPrevious.value + xCurrent.value;
-          xCurrent.value = 0;
-        }
-      }
+      xCurrent.value = 0;
+      yCurrent.value = 0;
 
       scaleCurrent.value = 1;
     });
