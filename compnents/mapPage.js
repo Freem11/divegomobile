2d3c7e3e520eee4c6b56483524427ff0295b602e
Freem11 <freem1985@gmail.com>
2024-08-15 06:51:47 -0600

    reinforced? onboarding trigger

--- compnents/mapPage.js
+++ compnents/mapPage.js
@@ -397,63 +397,12 @@ export default function MapPage() {
     }
   };
 
-  // const registerForPushNotificationsAsync = async (sess) => {
-  //   const { status: existingStatus } =
-  //     await Notifications.getPermissionsAsync();
-  //   let finalStatus = existingStatus;
-
-  //   if (existingStatus !== "granted") {
-  //     const { status } = await Notifications.requestPermissionsAsync();
-  //     finalStatus = status;
-  //   }
-
-  //   if (finalStatus !== "granted") {
-  //     return;
-  //   }
-
-  //   let token;
-  //   try {
-  //     token = (
-  //       await Notifications.getDevicePushTokenAsync({
-  //         projectId: Constants.expoConfig.extra.eas.projectId,
-  //       })
-  //     ).data;
-  //   } catch (err) {
-  //     console.log("error", err);
-  //   }
-
-  //   let tokenE;
-  //   try {
-  //     tokenE = (
-  //       await Notifications.getExpoPushTokenAsync({
-  //         projectId: Constants.expoConfig.extra.eas.projectId,
-  //       })
-  //     ).data;
-  //   } catch (err) {
-  //     console.log("error", err);
-  //   }
-
-  //   if (activeSession && activeSession.user) {
-  //     const user = await grabProfileById(activeSession.user.id);
-  //     const activeToken = user[0].expo_push_token;
-  //     if (activeToken === null || !activeToken.includes(token)) {
-  //       updatePushToken({
-  //         token: activeToken ? [...activeToken, token] : [token],
-  //         UserID: activeSession.user.id,
-  //       });
-  //     }
-  //     if (activeToken === null || !activeToken.includes(tokenE)) {
-  //       updatePushToken({
-  //         token: activeToken ? [...activeToken, tokenE] : [tokenE],
-  //         UserID: activeSession.user.id,
-  //       });
-  //     }
-  //   }
-  // };
-
   useLayoutEffect(() => {
     getProfile();
-    // registerForPushNotificationsAsync();
+  }, []);
+
+  useEffect(() => {
+    getProfile();
   }, []);
 
   useEffect(() => {
