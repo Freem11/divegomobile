a9bc463db6c74924d8045de3105627faf19ff003
freem11 <freem1985@gmail.com>
2024-09-11 17:34:34 -0600

    wavy uploader pulling form screen data file

--- compnents/screens/wavyHeaderUploader.js
+++ compnents/screens/wavyHeaderUploader.js
@@ -16,11 +16,7 @@ import screenData from "./screenData.json";
 import { chooseImageHandler } from "./imageUploadHelpers";
 import { moderateScale } from "react-native-size-matters";
 import Svg, { Path } from "react-native-svg";
-import {
-  colors,
-  primaryButtonAlt,
-  buttonTextAlt,
-} from "../styles";
+import { colors, primaryButtonAlt, buttonTextAlt } from "../styles";
 import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
@@ -73,46 +69,47 @@ export default function WavyHeaderUploader({
 
   return (
     <View style={styles.customStyles}>
-        {picUri ? (
-            <ImageBackground
-              source={{ uri: picUri }}
-              style={styles.backgroundImage}
-            />
-        ) : (
-            <View style={styles.uploaderBackground}>
-              <TouchableWithoutFeedback onPress={handleImageUpload}>
-                <View style={styles.photoUploadButton}>
-                  <Text style={styles.photoUploadText}>Upload a Photo</Text>
-                </View>
-              </TouchableWithoutFeedback>
-
+      {picUri ? (
+        <ImageBackground
+          source={{ uri: picUri }}
+          style={styles.backgroundImage}
+        />
+      ) : (
+        <View style={styles.uploaderBackground}>
+          <TouchableWithoutFeedback onPress={handleImageUpload}>
+            <View style={styles.photoUploadButton}>
+              <Text style={styles.photoUploadText}>
+                {screenData.PicUploader.uploadButton}
+              </Text>
             </View>
-        )}
-        <View
+          </TouchableWithoutFeedback>
+        </View>
+      )}
+      <View
+        style={{
+          flex: 1,
+          height: windowWidth > 600 ? "130%" : "100%",
+          backgroundColor: "white",
+        }}
+      >
+        <Svg
+          height={windowWidth > 600 ? "175%" : "100%"}
+          width={windowWidth > 600 ? "140%" : "100%"}
+          viewBox="0 0 1440 320"
           style={{
             flex: 1,
-            height: windowWidth > 600 ? "130%" : "100%",
-            backgroundColor: "white",
+            marginLeft: windowWidth > 600 ? "-20%" : 0,
+            marginTop: windowWidth > 600 ? "-102%" : "-80%",
+            backgroundColor: "transparent",
+            zIndex: 5,
           }}
         >
-          <Svg
-            height={windowWidth > 600 ? "175%" :"100%"}
-            width={windowWidth > 600 ? "140%" : "100%"}
-            viewBox="0 0 1440 320"
-            style={{
-              flex: 1,
-              marginLeft:  windowWidth > 600 ? "-20%" : 0,
-              marginTop: windowWidth > 600 ? "-102%" : "-80%",
-              backgroundColor: "transparent",
-              zIndex: 5,
-            }}
-          >
-            <Path
-              fill="#ffffff"
-              d="M 0,700 L 0,262 C 123.33333333333331,187.60000000000002 246.66666666666663,113.20000000000002 401,132 C 555.3333333333334,150.79999999999998 740.6666666666667,262.8 919,300 C 1097.3333333333333,337.2 1268.6666666666665,299.6 1540,102 L 1440, 2200 L 0,2200 Z"
-            />
-          </Svg>
-        </View>
+          <Path
+            fill="#ffffff"
+            d="M 0,700 L 0,262 C 123.33333333333331,187.60000000000002 246.66666666666663,113.20000000000002 401,132 C 555.3333333333334,150.79999999999998 740.6666666666667,262.8 919,300 C 1097.3333333333333,337.2 1268.6666666666665,299.6 1540,102 L 1440, 2200 L 0,2200 Z"
+          />
+        </Svg>
+      </View>
     </View>
   );
 }
