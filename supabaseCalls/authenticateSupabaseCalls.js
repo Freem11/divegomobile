e0d8601c12d5f8d44b36cab49350dbe9a4cb60b3
Vladimir Elisavetski <yvova88@yahoo.com>
2025-01-04 12:40:46 -0700

    updating fbsdk

--- supabaseCalls/authenticateSupabaseCalls.js
+++ supabaseCalls/authenticateSupabaseCalls.js
@@ -1,5 +1,5 @@
 import { supabase } from "../supabase";
-import { makeRedirectUri } from "expo-auth-session";
+// import { makeRedirectUri } from "expo-auth-session";
 
 export const sessionCheck = async() => {
   const session = await supabase.auth.getSession();
@@ -63,30 +63,30 @@ export const signInStandard = async (loginDetails) => {
   }
 };
 
-export const signInFaceBook = async () => {
-  const redirectTo = makeRedirectUri();
+// export const signInFaceBook = async () => {
+//   const redirectTo = makeRedirectUri();
 
-  console.log("hey", redirectTo)
-  if(redirectTo){
-    const { data, error } = await supabase.auth.signInWithOAuth({
-      provider: 'facebook',
-      options :{
-        redirectTo,
-        skipBrowserRedirect: true,
-      }
-    });
+//   console.log("hey", redirectTo)
+//   if(redirectTo){
+//     const { data, error } = await supabase.auth.signInWithOAuth({
+//       provider: 'facebook',
+//       options :{
+//         redirectTo,
+//         skipBrowserRedirect: true,
+//       }
+//     });
   
-    if (error) {
-      console.log("couldn't login,", error);
-    }
+//     if (error) {
+//       console.log("couldn't login,", error);
+//     }
   
-    if (user && session) {
-      // console.log(user, session);
-      return { user, session };
-    }
-  }
+//     if (user && session) {
+//       // console.log(user, session);
+//       return { user, session };
+//     }
+//   }
 
-};
+// };
 
 export const signInGoogle = async () => {
   const { user, session, error } = await supabase.auth.signIn({
