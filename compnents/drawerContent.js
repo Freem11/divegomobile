2c3c0c76e10bfb28318fa91b699a308b0160019e
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2025-01-16 19:31:57 -0700

    newMain: adjusted positioning of map button to be viewable on all devices

--- compnents/drawerContent.js
+++ compnents/drawerContent.js
@@ -39,7 +39,7 @@ const styles = StyleSheet.create({
     primaryButton,
     {
       position: 'absolute',
-      top: 800,
+      top: '80%',
       alignItems: "center",
       textAlign: "center",
       zIndex: 20,
