3542b0cdc996d209a0f4d3c9450343a0ca7dabcd
Kurt Ball <kurtisdball@hotmail.com>
2025-04-16 18:58:58 -0600

    get rid of unused useEffect

--- compnents/modals/picture.js
+++ compnents/modals/picture.js
@@ -215,26 +215,11 @@ export default function Picture(props) {
     }
   }
 
-  useEffect(() => {
-    let localUri = `https://divegolanding.web.app`;
-
-    const shareOptions = {
-      message: "",
-      url: "",
-    };
-    if (base64) {
-      shareOptions.message = `Checkout this cool pic of a ${creastureN} on Scuba SEAsons! It was taken by ${userN} at the dive site: ${selectedDiveSite.SiteName}, in${mapLocal} on ${photoDate}.\nMaybe we should start contributing our pics as well!\n\nLearn more about it here:\n${localUri}`;
-      shareOptions.url = `data:image/jpg;base64,${base64}`;
-      doShare(shareOptions);
-    }
-    setBase64(null);
-  }, [base64]);
-
-  const togglePhotoBoxModal = (photo) => {
-    setSelectedPhoto(photo);
-    setFullScreenModal(true);
-    setActiveTutorialID("PinchAndZoomPhoto");
-  };
+  // const togglePhotoBoxModal = (photo) => {
+  //   setSelectedPhoto(photo);
+  //   setFullScreenModal(true);
+  //   setActiveTutorialID("PinchAndZoomPhoto");
+  // };
 
   return (
     <View key={pic.id} style={styles.outterBox}>
