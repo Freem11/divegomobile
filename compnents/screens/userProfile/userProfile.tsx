04bf5710082db6d01a29bf67a6f7fb1ad0857530
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2025-07-16 18:12:44 -0600

    mobile-252-ShowAllScreens: removed unised code

--- compnents/screens/userProfile/userProfile.tsx
+++ compnents/screens/userProfile/userProfile.tsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from "react";
 import * as S from "./styles";
-import SeaLifeImageCard from "../../reusables/seaLifeImageCard/seaLifeImageCard";
-import Icon from "../../../icons/Icon";
 import Label from "../../reusables/label";
 import { Photo } from "../../../entities/photos";
-import { colors } from "../../styles";
 import { ActiveProfile } from "../../../entities/profile";
 import Button from "../../reusables/button";
 
@@ -35,20 +32,6 @@ export default function UserProfileScreenView({
     })
 
   },[selectedProfile])
-
-  const groupedPhotos = {};
-
-  profilePhotos && profilePhotos.forEach(photo => {
-    const key = `${photo.divesitename}_${photo.dateTaken}`;
-    if (!groupedPhotos[key]) {
-      groupedPhotos[key] = {
-        divesitename: photo.divesitename,
-        dateTaken: photo.dateTaken,
-        photos: [],
-      };
-    }
-    groupedPhotos[key].photos.push(photo);
-  });
 
   return (
     <S.ContentContainer>
@@ -82,38 +65,6 @@ export default function UserProfileScreenView({
         title={'View All'} 
         />
     </S.ButtonWrapper>
-
-      {/* {groupedPhotos && Object.values(groupedPhotos).map((photoPacket, index) => {
-  return (
-    <S.PhotoContainer key={`${photoPacket.id}-${index}`}>   
-      <S.PacketHeader key={`${photoPacket.id}-${index}`}>
-        <S.HeaderWrapper>
-          <S.IconWrapper>
-            <Icon name={'anchor'} fill={colors.primaryBlue}/>
-          </S.IconWrapper>
-          <S.PacketHeaderItem>{photoPacket.divesitename}</S.PacketHeaderItem>
-        </S.HeaderWrapper>
-        <S.HeaderWrapper>
-          <S.PacketHeaderDate>{photoPacket.dateTaken}</S.PacketHeaderDate>
-        </S.HeaderWrapper>
-      </S.PacketHeader>
-
-      {photoPacket.photos.length > 0 &&
-        photoPacket.photos.map((photo, index) => {
-          return (
-            <SeaLifeImageCard
-              key={`${photo.id}-${index}`}
-              pic={photo}
-              dataSetType={"ProfilePhotos"}
-              diveSiteName={photoPacket.name}
-              diveSiteAction={() => handleDiveSiteMove(photo, photoPacket)}
-            />
-          );
-        })}
-    </S.PhotoContainer>
-  ); */}
-{/* })} */}
-
     </S.ContentContainer>
   );
 }
