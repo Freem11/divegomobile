33f6146dd9612ead5abe5f27c1101806716b7492
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2025-08-25 19:05:29 -0600

    main: fixed sea life picture upload issue, updated code for better error handling and returns, updated how  aws keys are handled by eas so that they are seen in production

--- aws.js
+++ aws.js
@@ -2,15 +2,15 @@ import {
   S3Client,
 } from "@aws-sdk/client-s3";
 
-const awsAccountId = process.env.EXPO_PUBLIC_AWS_R2_ACCOUNT_ID
-const awsAccessKeyId = process.env.EXPO_PUBLIC_AWS_R2_ACCESS_KEY_ID
-const awsSecretKey = process.env.EXPO_PUBLIC_AWS_R2_SECRET_KEY
+const awsAccessKeyId = process.env.EXPO_PUBLIC_AWS_R2_ACCESS_KEY_ID || process.env.AWS_R2_ACCESS_KEY_ID;
+const awsSecretKey = process.env.EXPO_PUBLIC_AWS_R2_SECRET_KEY || process.env.AWS_R2_SECRET_KEY;
+const awsAccountId = process.env.EXPO_PUBLIC_AWS_R2_ACCOUNT_ID || process.env.AWS_R2_ACCOUNT_ID;
 
 export const aws3 = new S3Client({
-    region: "auto",
-    endpoint: `https://${awsAccountId}.r2.cloudflarestorage.com`,
-    credentials: {
-        accessKeyId : awsAccessKeyId,
-        secretAccessKey: awsSecretKey
-    }
-})
+  region: "auto",
+  endpoint: `https://${awsAccountId}.r2.cloudflarestorage.com`,
+  credentials: {
+    accessKeyId : awsAccessKeyId,
+    secretAccessKey: awsSecretKey
+  }
+});
---
+++ compnents/screens/imageUploadHelpers.ts
@@ -0,0 +1,82 @@
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+
+import { registerForPhotoLibraryAccessAsync } from "../tutorial/photoLibraryRegistery";
+import { uploadphoto } from "../cloudflareBucketCalls/cloudflareAWSCalls";
+import { showError } from "../toast";
+
+export const chooseImageHandler = async() => {
+  const permissionGiven = await registerForPhotoLibraryAccessAsync("yes");
+
+  if (!permissionGiven) {
+    return;
+  }
+
+  const chosenImage = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ["images", "videos"],
+    allowsEditing: true,
+    aspect: [4, 3],
+    quality: 1,
+    exif: true,
+  });
+
+  try {
+    return await chosenImage;
+  } catch (e) {
+    console.log({ title: "Image Upload", message: e.message });
+  }
+};
+
+export const imageUpload = async(image) => {
+  try {
+    const uriLink = image.assets[0].uri.uri;
+    const extension = uriLink.split(".").pop();
+    const fileName = `${Date.now()}.${extension}`;
+
+    const newFileUri = FileSystem.documentDirectory + fileName;
+
+    await FileSystem.moveAsync({
+      from: uriLink,
+      to: newFileUri,
+    });
+
+    const fileInfo = await FileSystem.readAsStringAsync(newFileUri, {
+      encoding: FileSystem.EncodingType.Base64,
+    });
+
+    await uploadphoto(fileInfo, fileName);
+
+    // showSuccess(`THISSSSSSSSSS: ${fileName}`);
+    return fileName;
+
+  } catch (error) {
+    showError(error.message);
+    console.error("Image upload failed:", error);
+    return null;
+  }
+};
+
+function formatDate(dateTaken) {
+
+  const slicedDate = dateTaken.substring(0, 10);
+  const formattedDate1 = slicedDate.replace(":", "-");
+  const formattedDate = formattedDate1.replace(":", "-");
+  return formattedDate;
+}
+
+function createFile(dateTaken, fileSize) {
+
+  const fileName = dateTaken.substring(
+    dateTaken.lastIndexOf("/") + 1,
+    dateTaken.length
+  );
+
+  const fileToUpload = {
+    uri: dateTaken,
+    name: fileName,
+    type: "image/jpg",
+    fileSize: fileSize,
+  };
+  return fileToUpload;
+}
+export { formatDate, createFile };
