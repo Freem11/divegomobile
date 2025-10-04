998c91a796a70a371c08414b60c511d22d759e3f
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2025-10-04 09:31:36 -0600

    feature/navigation_map_flips: return to show button now returns correctly

--- compnents/googleMap/navigation/returnToShopButton/index.tsx
+++ compnents/googleMap/navigation/returnToShopButton/index.tsx
@@ -1,13 +1,14 @@
 import { StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
+import { useContext } from "react";
+
 import { buttonTextAlt, primaryButtonAlt } from "../../../styles";
 import { useMapStore } from "../../useMapStore";
 import { useActiveScreenStore } from "../../../../store/useActiveScreenStore";
 import { getDiveShopById } from "../../../../supabaseCalls/shopsSupabaseCalls";
-import { useContext } from "react";
 import { LevelOneScreenContext } from "../../../contexts/levelOneScreenContext";
 import { SitesArrayContext } from "../../../contexts/sitesArrayContext";
 import { getDiveSiteById } from "../../../../supabaseCalls/diveSiteSupabaseCalls";
-
+import { useAppNavigation } from "../../../mapPage/types";
 
 const styles = StyleSheet.create({
   lowerButtonText: buttonTextAlt,
@@ -22,43 +23,46 @@ const styles = StyleSheet.create({
 });
 
 export function ReturnToShopButton() {
+  const navigation = useAppNavigation();
   const mapRef = useMapStore((state) => state.mapRef);
   const navProps = useMapStore((state) => state.navProps);
   const setActiveScreen = useActiveScreenStore((state) => state.setActiveScreen);
   const setMapConfig = useMapStore((state) => state.actions.setMapConfig);
   const { setSitesArray } = useContext(SitesArrayContext);
   const { setLevelOneScreen } = useContext(LevelOneScreenContext);
-  
+
   const onPress = async () => {
-    if (navProps.pageName === "DiveSite"){
-      const diveSiteInfo = await getDiveSiteById(navProps.itemId)
+    if (navProps.pageName === "DiveSite") {
+      const diveSiteInfo = await getDiveSiteById(navProps.itemId);
       setLevelOneScreen(true);
-      setActiveScreen("DiveSiteScreen", navProps.itemId);
+      navigation.navigate("DiveSite", { id: navProps.itemId });
+      // setActiveScreen("DiveSiteScreen", navProps.itemId);
       mapRef.animateCamera({
-        center: {latitude: diveSiteInfo[0].lat, longitude: diveSiteInfo[0].lng},
+        center: { latitude: diveSiteInfo[0].lat, longitude: diveSiteInfo[0].lng },
         zoom: 12,
       });
     } else {
-      const diveCentreinfo = await getDiveShopById(navProps.itemId)
+      const diveCentreinfo = await getDiveShopById(navProps.itemId);
       setLevelOneScreen(true);
-      setActiveScreen("DiveShopScreen", navProps.itemId);
+      navigation.navigate("DiveCentre", { id: navProps.itemId });
+      // setActiveScreen("DiveShopScreen", navProps.itemId);
       mapRef.animateCamera({
-        center: {latitude: diveCentreinfo[0].lat, longitude: diveCentreinfo[0].lng},
+        center: { latitude: diveCentreinfo[0].lat, longitude: diveCentreinfo[0].lng },
         zoom: 12,
       });
     }
- 
-    setMapConfig(0, {pageName: '', itemId: 0});
+
+    setMapConfig(0, { pageName: "", itemId: 0 });
     setSitesArray([]);
   };
 
   return (
     <TouchableWithoutFeedback onPress={onPress}>
       <View style={styles.lowerButtonWrapper}>
-                  <Text style={styles.lowerButtonText}>
-                    {"Return to Shop"}
-                  </Text>
-                </View>
+        <Text style={styles.lowerButtonText}>
+          {"Return to Shop"}
+        </Text>
+      </View>
     </TouchableWithoutFeedback>
   );
 }
