412b2144d5ae0e72a942efe33f4832fbffb75981
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2025-10-14 09:48:44 -0600

    mobile-288-edtQueries: added minutesAgo record age check that will allow us to delete after new records have been confirmed aadded to the db

--- supabaseCalls/diveSiteReviewCalls/deletes.ts
+++ supabaseCalls/diveSiteReviewCalls/deletes.ts
@@ -1,10 +1,14 @@
 import { supabase } from "../../supabase";
 
 export const deleteReviewConditions = async(review_id: number) => {
+  //set to one minute ago
+  const minutesAgo = new Date(Date.now() - 60000).toISOString();
+
   const { data, error } = await supabase
     .from("diveSiteReviewConditions")
     .delete()
-    .eq("review_id", review_id);
+    .eq("review_id", review_id)
+    .lt("created_at", minutesAgo);
 
   if (error) {
     console.log("couldn't do it REVIEW_CONDITIONS_DELETE,", error);
@@ -17,10 +21,14 @@ export const deleteReviewConditions = async(review_id: number) => {
 };
 
 export const deleteReviewPhotos = async(review_id: number) => {
+  //set to one minute ago
+  const minutesAgo = new Date(Date.now() - 60000).toISOString();
+
   const { data, error } = await supabase
     .from("diveSiteReviewPhotos")
     .delete()
-    .eq("review_id", review_id);
+    .eq("review_id", review_id)
+    .lt("created_at", minutesAgo);
 
   if (error) {
     console.log("couldn't do it REVIEW_PHOTO_DELETE,", error);
