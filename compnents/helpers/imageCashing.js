228536b5cf50838321af023143f54b05633ef81a
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2025-10-27 22:18:03 -0600

    feature/diveSiteReviewsStats: fixed app.ts bable config and naviagtion deps to cure android crashing issue

--- compnents/helpers/imageCashing.js
+++ compnents/helpers/imageCashing.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Image } from "react-native";
-import * as FileSystem from "expo-file-system";
+import * as FileSystem from "expo-file-system/legacy";
 
 import cloudflareBucketUrl from "../globalVariables";
 import noImage from "../png/NoImage.png"; // image module
