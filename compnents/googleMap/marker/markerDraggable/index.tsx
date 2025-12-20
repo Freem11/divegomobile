b346904d3c0f260a74cd3d7a16b331066797e46b
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2025-12-20 09:52:45 -0700

    staging: cleaned out old unused icons

--- compnents/googleMap/marker/markerDraggable/index.tsx
+++ compnents/googleMap/marker/markerDraggable/index.tsx
@@ -1,35 +1,29 @@
-import React, { useContext, useEffect, useState } from "react";
-import { Marker } from "react-native-maps";
-import { useMapStore } from "../../useMapStore";
-import { Coordinates } from "../../../../entities/coordinates";
-import icon from "../../../png/mapIcons/Manta_60.png";
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
 
-import { Image, StyleSheet, Text, View } from "react-native";
+import icon from "../../../png/mapIcons/Manta_60.png";
 
 const styles = StyleSheet.create({
   markerFixed: {
-    left: '50%',
+    left: "50%",
     marginLeft: -20,
     marginTop: -48,
-    position: 'absolute',
-    top: '50%'
+    position: "absolute",
+    top: "50%"
   },
   marker: {
     height: 48,
     width: 48
   }
-})
+});
 
-type MarkerDraggableProps = {
-};
-
-export function MarkerDraggable(props: MarkerDraggableProps) {
+export function MarkerDraggable() {
 
   return (
 
-  <View style={styles.markerFixed}>
-    <Image style={styles.marker} source={icon} />
-  </View>
+    <View style={styles.markerFixed}>
+      <Image style={styles.marker} source={icon} />
+    </View>
   );
 }
 
