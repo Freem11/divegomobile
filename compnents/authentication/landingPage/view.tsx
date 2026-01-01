f551fa5e5307a9b4cf6b4971b567ef0495030e6b
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2026-01-01 10:24:23 -0700

    landingPageExperiment: reverted to pitures for now

--- compnents/authentication/landingPage/view.tsx
+++ compnents/authentication/landingPage/view.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   Image,
   TouchableWithoutFeedback,
   Keyboard,
-  StyleSheet,
+  FlatList,
+  Dimensions,
 } from "react-native";
 import { useTranslation } from "react-i18next";
 import Svg, { Path } from "react-native-svg";
-import Video from "react-native-video";
 
 import googleLogo from "../../png/loginIcons/google.png";
 import facebookLogo from "../../png/loginIcons/facebook.png";
@@ -15,6 +15,9 @@ import appleLogo from "../../png/loginIcons/apple.png";
 import Button from "../../reusables/button";
 
 import * as S from "./styles";
+
+// Get screen dimensions for the FlatList items
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 interface IProps {
   isSignedIn: boolean;
@@ -25,6 +28,15 @@ interface IProps {
   onFacebook: () => void;
   onApple: () => void;
 }
+
+// Define your image array here
+const BACKGROUND_IMAGES = [
+  require("../../png/SeaHorse.jpg"),
+  require("../../png/SeaTurt.jpg"),
+  require("../../png/SeaLion2.jpg"),
+  require("../../png/Octos.jpg"),
+  require("../../png/Manta.jpg"),
+];
 
 const CustomWave = () => (
   <Path
@@ -40,22 +52,55 @@ export default function LandingPageView(props: IProps) {
   const { t } = useTranslation();
   const facebookAuthAvailable = false;
 
+  const [activeIndex, setActiveIndex] = useState(0);
+  const flatListRef = useRef<FlatList>(null);
+
+  // Auto-scroll logic
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let nextIndex = activeIndex + 1;
+
+      if (nextIndex >= BACKGROUND_IMAGES.length) {
+        nextIndex = 0;
+      }
+
+      flatListRef.current?.scrollToIndex({
+        index: nextIndex,
+        animated: true,
+      });
+
+      setActiveIndex(nextIndex);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [activeIndex]);
+
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
       <S.Container>
 
-        {/* Background Video Section */}
+        {/* Animated Background Section */}
         <S.BackgroundContainer>
-          <Video
-            source={{ uri: "https://pub-9114df4c0fd044d0806a9e8819aa3212.r2.dev/HeroVideo2026-1.mov" }}
-            style={styles.backgroundVideo}
-            muted={true}
-            repeat={true}
-            resizeMode="cover"
-            rate={1.0}
-            ignoreSilentSwitch={"obey"}
-            playWhenInactive={false}
-            playInBackground={false}
+          <FlatList
+            ref={flatListRef}
+            data={BACKGROUND_IMAGES}
+            keyExtractor={(_, index) => index.toString()}
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+            scrollEnabled={false} // Prevents user interaction from fighting the timer
+            getItemLayout={(_, index) => ({
+              length: SCREEN_WIDTH,
+              offset: SCREEN_WIDTH * index,
+              index,
+            })}
+            renderItem={({ item }) => (
+              <Image
+                source={item}
+                style={{ width: SCREEN_WIDTH, height: SCREEN_HEIGHT / 2 }}
+                resizeMode="cover"
+              />
+            )}
           />
         </S.BackgroundContainer>
 
@@ -125,13 +170,3 @@ export default function LandingPageView(props: IProps) {
     </TouchableWithoutFeedback>
   );
 }
-
-const styles = StyleSheet.create({
-  backgroundVideo: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-  },
-});
